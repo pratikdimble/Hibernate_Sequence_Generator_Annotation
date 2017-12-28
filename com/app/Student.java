a37dc571b4184d5d8c504818af77d9c8ca4b1593
Pratik Mahadev Dimble <31777260+pratikdimble@users.noreply.github.com>
2017-12-28 11:41:49 +0530

    Create Student.java

---
+++ com/app/Student.java
@@ -0,0 +1,65 @@
+
+package com.app;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="STUDENT_FORM")
+public class Student {
+	
+	private int sid;
+	private String sname;
+	private String semail;
+	private String sresult;
+	
+	@Id
+	@Column(name="ID",length=10)
+	//@GeneratedValue(strategy=GenerationType.SEQUENCE)
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="my_seq")
+	@SequenceGenerator(
+	    name="my_seq",
+	    sequenceName="studsequence",
+	    allocationSize=20
+	)
+	public int getSid() {
+		return sid;
+	}
+	public void setSid(int sid) {
+		this.sid = sid;
+	}
+	@Column(name="Name")
+	public String getSname() {
+		return sname;
+	}
+	public void setSname(String sname) {
+		this.sname = sname;
+	}
+	@Column(name="EMail")
+	public String getSemail() {
+		return semail;
+	}
+	public void setSemail(String semail) {
+		this.semail = semail;
+	}
+	@Column(name="Result")
+	public String getSresult() {
+		return sresult;
+	}
+	public void setSresult(String sresult) {
+		this.sresult = sresult;
+	}
+	
+	@Override
+	public String toString() {
+		return "Student [sid=" + sid + ", sname=" + sname + ", semail=" + semail + ", sresult=" + sresult + "]";
+	}
+	
+	
+
+}
